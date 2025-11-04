491407ab2fdb1a1df25e4f70abd43d0a0ef813ad
Cameron Shiu <cameronshiu@gmail.com>
2025-11-04 10:18:10 -0800

    Rename config to mjs for Vercel

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // needed for Next.js App Router
+  },
+}
+
+export default nextConfig
